c3e13a41a60dd4524472c5825d281753e1aba62e
Shahria Abeed <02sabeed@gmail.com>
2023-04-03 18:49:21 -0400

    Made blog the home page

--- myWebApp/myWebApp/urls.py
+++ myWebApp/myWebApp/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# this is home for the whole project
+# all relating sites are connected through here
+# 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
