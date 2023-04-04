c4bc3995f1e1a01dd9f18572b96f7edb4cd99e8f
Shahria Abeed <02sabeed@gmail.com>
2023-04-04 18:44:23 -0400

    Added Templates & some html

--- myWebApp/blog/views.py
+++ myWebApp/blog/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# dummy posts
+posts = [
+    {
+        'author': 'anAuthor',
+        'title': 'First Blog!',
+        'content': 'First!',
+        'date_posted': '4/4/23'
+    },
+    {
+        'author': 'newGuy',
+        'title': '2ND Blog!',
+        'content': '2ND!',
+        'date_posted': '4/5/23'
+    }
+]
 
 # handle traffic from home page of blog
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    context = {
+        'title': About
+    }
+    return render(request, 'blog/about.html', context)
 # Create your views here.
